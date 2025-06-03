3d6636280e6d1277e4cf41529e5d1a55b7fbfaa0
shaaav1e <shaaavie@gmail.com>
2025-06-03 13:55:00 +0500

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-slate-900 text-green-500">ss</div>
+    </>
+  );
+}
+
+export default App;
